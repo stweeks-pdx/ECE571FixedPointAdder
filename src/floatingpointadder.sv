c6a041ba58dbb6cb18c993d7327398df230997f1
Hector Cardenas <hector7@auto.ece.pdx.edu>
2024-05-10 17:20:23 -0700

    Testing rebase on local system before completing skeleton

--- src/floatingpointadder.sv
+++ src/floatingpointadder.sv
@@ -9,5 +9,18 @@ module FloatingPointAdder(input float AddendA,
 			  output logic Nan);
 parameter VERSION = "0.1";
 
+//*** TENTATIVE MODULE SKELETON ***
+
+// Small ALU
+// Takes in exponents from AddendA and AddendB
+// Outputs exponent difference
+
+// Exponent MUX
+// Takes in exponents from AddendA and AddendB
+// Takes in select from Control FSM
+// Outputs value of larger exponent 
+
+
+
 endmodule
 
