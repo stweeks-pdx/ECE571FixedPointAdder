a0c969c7923f5b70e9c5ff55538a8763676fbe03
Stephen Weeks <stweeks@mo.ece.pdx.edu>
2024-05-14 18:52:20 -0700

    Exponent ALU Code

---
+++ src/expalu.sv
@@ -0,0 +1,22 @@
+module ExpALU(ExpA, ExpB, ExpSet, ExpDiff);
+parameter N = 8;
+localparam BIAS = (2**N)/2;
+localparam MSB = N - 1;
+
+input [MSB:0] ExpA, ExpB;
+output [MSB:0] ExpDiff;
+output ExpSet;
+
+logic signed [MSB:0] DeBiasedExpA, DeBiasedExpB;
+
+// Circuit to remove exponent bias
+assign DeBiasedExpA = ExpA - BIAS;
+assign DeBiasedExpB = ExpB - BIAS;
+
+// Comparator Circuit for Exponents
+assign ExpSet = (DeBiasedExpA >= DeBiasedExpB) ? 1 : 0;
+
+// Subtractor Circuit for Exponents
+assign ExpDiff = (ExpSet ? DeBiasedExpA - DeBiasedExpB : DeBiasedExpB - DeBiasedExpA);
+
+endmodule
