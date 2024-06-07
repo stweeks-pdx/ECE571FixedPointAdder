210e78fe975cf05983c5eda5fb271c5200ad2f2b
Stephen Weeks <stweeks@mo.ece.pdx.edu>
2024-06-06 21:45:28 -0700

    Fixing big alu math

--- src/bigalu.sv
+++ src/bigalu.sv
@@ -8,28 +8,31 @@ output [MSB:0] Result;
 output ccc, ccz, ccv, ccn;
 
 // internal signals
-logic [MSB:0] FracA2sC, FracB2sC;
-logic [MSB:0] AddendA, AddendB;
+logic [N:0] FracA2sC, FracB2sC;
+logic [N:0] AddendA, AddendB;
+logic [N:0] MidResult;
 logic cb;
 
 // create 2's compliment for the mux
-assign FracA2sC = ~FracA + 1'b1;
-assign FracB2sC = ~FracB + 1'b1;
+assign FracA2sC = ~{SignA, FracA} + 1'b1;
+assign FracB2sC = ~{SignB, FracB} + 1'b1;
 
-mux mA(FracA2sC, FracA, SignA, AddendA);
-mux mB(FracB2sC, FracB, SignB, AddendB);
+mux mA(FracA2sC, {1'b0,FracA}, SignA, AddendA);
+mux mB(FracB2sC, {1'b0,FracB}, SignB, AddendB);
 
 // ALU addition + flags
-assign {cb,Result} = AddendA + AddendB;
+assign {cb,MidResult} = AddendA + AddendB;
 assign ccc = cb;
-assign ccz = (Result[MSB:0] == 0); // (not sure this is correct)
-assign ccv = (Result[MSB] != AddendA[MSB]) && (AddendA[MSB] == AddendB[MSB]);
-assign ccn = Result[MSB];
+assign ccz = (MidResult[N:0] == 0); // (not sure this is correct)
+assign ccv = (MidResult[N] != AddendA[N]) && (AddendA[N] == AddendB[N]);
+assign ccn = MidResult[N];
+
+assign Result = (ccn ? ~MidResult[MSB:0] : MidResult[MSB:0]);
 
 endmodule
 
 module mux(A, B, S, Y);
-parameter BITS = 24;
+parameter BITS = 25;
 localparam MSB = BITS - 1;
 
 input [MSB:0] A, B;
